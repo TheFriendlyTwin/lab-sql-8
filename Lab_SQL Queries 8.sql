954a92e5a860642432648274cd19e0c000622141
Mafalda Sousa <180323044@estudantes.ips.pt>
2023-12-16 09:40:02 +0000

    Mafalda commiting changes to Lab | SQL 8

--- Lab_SQL Queries 8.sql
+++ Lab_SQL Queries 8.sql
@@ -21,16 +21,23 @@ group by c.name;
 
 /* 4. Which actor has appeared in the most films? 
 Hint: You can create a join between the tables "actor" and "film actor" and count the number of times an actor appears. */
-select concat(a.first_name, ' ', a.last_name) as 'Actor', count(f.film_id) as 'Number of film appearances'
+select a.actor_id, concat(a.first_name, ' ', a.last_name) as 'Actor', count(f.film_id) as 'Number of film appearances'
 from sakila.actor a
 join sakila.film_actor f on  a.actor_id = f.actor_id
-group by a.first_name, a.last_name
+group by a.actor_id, a.first_name, a.last_name
 order by count(f.film_id) desc;
 
-select concat(a.first_name, ' ', a.last_name) as 'Actor', count(f.film_id) as 'Number of film appearances'
+select a.actor_id, concat(a.first_name, ' ', a.last_name) as 'Actor', count(f.film_id) as 'Number of film appearances'
 from sakila.actor a
 join sakila.film_actor f on  a.actor_id = f.actor_id
-group by a.first_name, a.last_name
+group by a.actor_id, a.first_name, a.last_name
+order by count(f.film_id) desc
+limit 1;
+
+select a.actor_id, concat(a.first_name, ' ', a.last_name) as 'Actor', count(f.film_id) as 'Number of film appearances'
+from sakila.actor a
+join sakila.film_actor f using (actor_id)
+group by a.actor_id, a.first_name, a.last_name
 order by count(f.film_id) desc
 limit 1;
 
@@ -49,6 +56,13 @@ group by c.customer_id, c.first_name, c.last_name
 order by count(r.rental_id) desc
 limit 1;
 
+select c.customer_id, concat(c.first_name, ' ', c.last_name) as 'Customer Name', count(r.rental_id) 'Number of rentals'
+from sakila.customer c
+join sakila.rental r using (customer_id)
+group by c.customer_id, c.first_name, c.last_name
+order by count(r.rental_id) desc
+limit 1;
+
 /* Bonus: Which is the most rented film? (The answer is Bucket Brotherhood).
 This query might require using more than one join statement. Give it a try. We will talk about queries with multiple join statements later in the lessons.
 Hint: You can use join between three tables - "Film", "Inventory", and "Rental" and count the rental ids for each film. */
@@ -63,6 +77,14 @@ select i.film_id, f.title, count(r.inventory_id) 'Number of rentals'
 from sakila.film f 
 join sakila.inventory i on f.film_id = i.film_id
 join sakila.rental r on i.inventory_id = r.inventory_id
+group by i.film_id, f.title
+order by count(r.inventory_id) desc
+limit 1;
+
+select i.film_id, f.title, count(r.inventory_id) 'Number of rentals'
+from sakila.film f 
+join sakila.inventory i using (film_id)
+join sakila.rental r using (inventory_id)
 group by i.film_id, f.title
 order by count(r.inventory_id) desc
 limit 1;
